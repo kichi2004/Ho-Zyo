84b6a1c3adae4afb29a492d92856b93a8a8ebff6
kvin-MisterD <kvin.MisterD@gmail.com>
2019-06-14 17:32:48 +0900

    Create HttpLister

--- Ho-Zyo/Program.cs
+++ Ho-Zyo/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace Ho_Zyo
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private readonly string _port = Environment.GetEnvironmentVariable("PORT");
+        private async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var url = $"http://localhost:{_port}/";
+            
+            using (var listener = new HttpListener())
+            {
+                listener.Prefixes.Add(url);
+                listener.Start();
+                while (true)
+                {
+                    var context = await listener.GetContextAsync();
+                    using (var res = context.Response)
+                    using (var stream = res.OutputStream)
+                    using (var writer = new StreamWriter(stream))
+                    {
+                        await writer.WriteLineAsync("Ho-Zyo is Available!");
+                    }
+                }
+            }
         }
     }
 }

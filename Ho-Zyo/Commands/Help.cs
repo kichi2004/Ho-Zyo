9580adcac07538cd777abd330806580e907befd4
kvin-MisterD <kvin.MisterD@gmail.com>
2019-06-14 21:53:05 +0900

    Refactoring

--- Ho-Zyo/Commands/Help.cs
+++ Ho-Zyo/Commands/Help.cs
@@ -10,10 +10,12 @@ namespace Ho_Zyo.Commands
         {
             var descriptions = new[]
             {
-                "```", "slot [num] -- 絵文字でスロットを回します。[num]回回します。未指定の場合は1。上限回数は10回です。",
+                "```",
+                "slot [num] -- 絵文字でスロットを回します。[num]回回します。未指定の場合は1。上限回数は10回です。",
                 "establish -- これまで成功した確率を出します。slotが成功した時に自動で実行されます。",
                 "help -- コマンドリストと説明を表示します。",
-                "寿司 -- ( '-' 🍣)ｽｼﾊﾟﾝﾁします。", "```"
+                "寿司 -- ( '-' 🍣)ｽｼﾊﾟﾝﾁします。",
+                "```"
             };
 
             await ReplyAsync(string.Join("\n", descriptions));

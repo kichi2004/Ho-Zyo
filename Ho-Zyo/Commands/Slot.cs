cb6125d177b84bf0c0fc489b56c2b7deb35171ab
kvin-MisterD <kvin.MisterD@gmail.com>
2019-06-15 17:58:11 +0900

    Fix establish

--- Ho-Zyo/Commands/Slot.cs
+++ Ho-Zyo/Commands/Slot.cs
@@ -10,8 +10,9 @@ namespace Ho_Zyo.Commands
     public class Slot : ModuleBase
     {
         private readonly List<Emoji> _emojis = Emoji.GetEmojis().Where(x => x.Animated == false).ToList();
-        private int _callCount = 0;
-        private int _achievementCount = 0;
+        private static int _callCount;
+        private static int _achievementCount;
+        private readonly Random _random = new Random();
 
         [Command("slot")]
         public async Task DoSlot(int num = 1)
@@ -32,9 +33,10 @@ namespace Ho_Zyo.Commands
             var send = new List<string>
             {
                 Context.User.Mention
+            #if DEBUG
+              , "[Debug Message]"
+            #endif
             };
-
-            var random = new Random();
 
             for (int j = 0; j < num; j++)
             {
@@ -42,33 +44,32 @@ namespace Ho_Zyo.Commands
 
                 for (int i = 0; i < 3; i++)
                 {
-                    var emoji = _emojis[random.Next(_emojis.Count - 1)];
+                    var emoji = _emojis[_random.Next(_emojis.Count - 1)];
                     chosen.Add(emoji.ToString());
                 }
 
                 send.Add(string.Join(' ', chosen));
                 _callCount++;
 
-                if (chosen.All(x => x == chosen.First()))
-                {
-                    send.Add($"おめでとうございます! {chosen.First()}");
-                    _achievementCount++;
-                }
+                if (chosen.Any(x => x != chosen.First())) continue;
+                send.Add($"おめでとうございます! {chosen.First()}");
+                _achievementCount++;
             }
 
-            send.Add($"{GetEstablishment():F5} ({_achievementCount}/{_callCount})");
+            send.Add(GetEstablishment());
             await ReplyAsync(string.Join("\n", send));
         }
 
-        private double GetEstablishment()
+        private string GetEstablishment()
         {
-            return _achievementCount == 0 ? 0 : (double) _achievementCount / _callCount;
+            var establish = _achievementCount == 0 ? 0 : (double) _achievementCount / _callCount;
+            return $"{establish * 100:F2} ({_achievementCount}/{_callCount})";
         }
 
         [Command("establish")]
-        public async Task Establishment()
+        public async Task SendEstablishment()
         {
-            await ReplyAsync($"{GetEstablishment():F5} ({_achievementCount}/{_callCount})");
+            await ReplyAsync(GetEstablishment());
         }
     }
 }
